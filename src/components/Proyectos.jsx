01e53c9e1caf9cab16a400a7f32754d3d3513854
Dailin Romero <dailinromero123@gmail.com>
2024-06-20 22:19:06 -0500

    array de proyectos archivo aparte

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,38 +1,9 @@
 import React from 'react'
 import { ListProyects } from './ListProyects'
 import '../css/proyectos.css'
+import { trabajos } from '../data/trabajos'
 
 export const Proyectos = () => {
-
-  const dataProyect = [
-    {
-      id: 1,
-      image: '../public/monitorias.jpg',
-      titulo: 'Easy-tutos',
-      descripcion: 'Plataforma para monitorias academicas en la universidad del magdalena',
-      lenguajes: ['html', 'css', 'react', 'php'],
-      link: '',
-      github: ''
-    },
-    {
-      id: 2,
-      image: '../public/blog-web.jpg',
-      titulo: 'Blog articulos',
-      descripcion: 'Pagina web donde se pueden publicar articulos',
-      lenguajes: ['html', 'css', 'react', 'php'],
-      link: '',
-      github: ''
-  },
-    {
-      id: 3,
-      image: '../public/aero-proy.jpg',
-      titulo: 'Rutas aeropuerto',
-      descripcion: 'Proyecto que ayuda a escoger mejor ruta al usuario',
-      lenguajes: ['html', 'css', 'react', 'php'],
-      link: '',
-      github: ''
-    },
-  ];
 
   return (
     <div className='page'>
@@ -41,8 +12,8 @@ export const Proyectos = () => {
 
         <div className='cont-proy'>
 
-            {dataProyect.map((dataProyect) => (
-              <ListProyects key={dataProyect.id} data={dataProyect} />
+            {trabajos.map((trabajos) => (
+              <ListProyects key={trabajos.id} data={trabajos} />
             ))}
         </div>
         

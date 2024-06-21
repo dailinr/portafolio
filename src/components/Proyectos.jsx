8cc1b979899f425ee218db1004fc6e8edfcfc2c9
Dailin Romero <dailinromero123@gmail.com>
2024-06-20 22:25:02 -0500

    proyectos unificacion componente

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { ListProyects } from './ListProyects'
 import '../css/proyectos.css'
 import { trabajos } from '../data/trabajos'
 
@@ -8,15 +7,40 @@ export const Proyectos = () => {
   return (
     <div className='page'>
 
-        <h3>Proyectos<hr/> </h3>
+      <h3>Proyectos<hr/> </h3>
 
-        <div className='cont-proy'>
+      <div className='cont-proy'>
 
-            {trabajos.map((trabajos) => (
-              <ListProyects key={trabajos.id} data={trabajos} />
-            ))}
-        </div>
-        
+        {trabajos.map((trabajos) => (
+
+          <div key={trabajos.id} className="card">
+            <div className="card-image">
+              <img src={trabajos.image} alt="image proyecto" />
+            </div>
+
+            <div className="card-content">
+
+              <h3 className="card-title">{trabajos.titulo}</h3>
+
+              <p className="card-description">
+                {trabajos.descripcion}
+              </p>
+
+              <button className="card-button mt-2 d-flex">Ver</button>
+              
+              <div className='lenguajes'>
+                <i className='bx bxl-html5' style={{color: '#E34F26'}}></i>
+                <i className='bx bxl-css3' style={{color: '#1572B6'}}></i>
+                <i className='bx bxl-react' style={{color: '#61DAFB'}}></i>
+                <i className='bx bxl-php' style={{color: '#777BB4'}}></i>
+              </div>
+              
+            </div>
+          </div>
+
+        ))}
+
+      </div>
     </div>
   )
 }
